c8f23ecc8476f967c29ad22193a0b79aeb2b0283
AbdoH7 <abdo.ah1102@gmail.com>
2022-12-23 23:11:56 +0200

    sitting the env file & updating firebase config

--- global/firebaseConf.js
+++ global/firebaseConf.js
@@ -1,13 +1,13 @@
 import { initializeApp } from 'firebase/app';
 
 export const firebaseConfig = {
-    apiKey: "AIzaSyB3axxAfqvWjp6oxKhwiqlNgRF-CAkZ5f8",
-    authDomain: "smartlock-d73a3.firebaseapp.com",
-    projectId: "smartlock-d73a3",
-    storageBucket: "smartlock-d73a3.appspot.com",
-    messagingSenderId: "239743663304",
-    appId: "1:239743663304:web:48e14b64c422139d7b79fd",
-    measurementId: "G-TBGY988EY8"
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId,
+    measurementId: process.env.measurementId
   };
 
   export const app = initializeApp(firebaseConfig);
