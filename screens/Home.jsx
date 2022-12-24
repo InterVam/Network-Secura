97caf4b534d79f5d5597d98110b49c55bb32e2bf
AbdoH7 <85498099+AbdoH7@users.noreply.github.com>
2022-12-24 02:30:15 +0200

    Added failure alert and removed unused imports (#6)

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -1,5 +1,5 @@
 import styled from "styled-components/native";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { View } from "react-native";
 import { getAuth  } from "firebase/auth";
 import {
     ThemeInput,
@@ -43,7 +43,6 @@ const Home = ({ navigation }) => {
         setData(docSnap.data())
       } else {
         // doc.data() will be undefined in this case
-        console.log("No such document!");
       }
     }
     useEffect(()=>{
@@ -73,13 +72,16 @@ const Home = ({ navigation }) => {
     client.connect({
       useSSL: false,
       timeout: 3,
-      onFailure:(()=>{console.log("Fail")}),
+      onFailure:(()=>{
+        Toast.show("Failed to connect to the server",{
+          type:"danger",
+          animationType: "slide-in"
+        });
+      }),
       userName:data?.Duser,
       password:data?.Dpass,
       onSuccess:(()=>{
-        console.log("yoooooooooo")
       if(/^\d+$/.test(pass) && pass.length == 6) {
-        console.log("yay")
         var message = new Paho.MQTT.Message(pass);
         message.destinationName = "pass";
         message.qos = 2;
@@ -90,13 +92,11 @@ const Home = ({ navigation }) => {
           animationType: "slide-in"
         });
       } else {
-        console.log("nay")
         Toast.show("Password can only contain 6 digits",{
           type:"danger",
           animationType: "slide-in"
         });
       }
-      
     })
   })}
 
